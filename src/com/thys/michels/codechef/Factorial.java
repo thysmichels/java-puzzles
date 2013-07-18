4a5da8d6fbcd8c31530a048088db224f50893066
Thys Michels <thysmichels@gmail.com>
2013-07-17 22:20:28 -0700

    Update to Factorial

--- src/com/thys/michels/codechef/Factorial.java
+++ src/com/thys/michels/codechef/Factorial.java
@@ -1,27 +1,34 @@
 package com.thys.michels.codechef;
 
 import java.io.BufferedInputStream;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import java.math.BigInteger;
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Factorial {
 	public static void main(String[] args) {
-		String data = "3\n 60 \n";
-		InputStream stdin = System.in;
-		System.setIn(new ByteArrayInputStream(data.getBytes()));
-		Scanner scanner = new Scanner(System.in);
-		System.setIn(stdin);
-		//Scanner scanner = new Scanner(new BufferedInputStream(System.in), "UTF-8");
+		Scanner scanner = new Scanner(new BufferedInputStream(System.in), "UTF-8");
 		String[] fields = scanner.useDelimiter("\\A").next().trim().split("[\\r\\n]+");
-		BigInteger n = BigInteger.ONE;
-        
 		for (String field : fields){
-			for (int i=1; i<=Double.valueOf(field); i++) {
-	            n = n.multiply(BigInteger.valueOf(i));
-	        }
+			System.out.println(countZeros(Integer.parseInt(field.trim())));
 		}
+	}
+	public static BigInteger factorial(int number) {
+		BigInteger ans = new BigInteger("1");
+		while (number > 0) {
+			ans = ans.multiply(new BigInteger(Integer.toString(number)));
+			number -= 1;
+		}
+		return ans;
+	}
+	public static int countZeros(int number) {
+		final BigInteger zero = new BigInteger("0");
+		final BigInteger ten = new BigInteger("10");
+		BigInteger fact = factorial(number);
+		int zeroCount = 0;
+		while (fact.mod(ten).compareTo(zero) == 0){
+			fact = fact.divide(ten);
+			zeroCount += 1;
+		}
+		return zeroCount;
 	}
 }

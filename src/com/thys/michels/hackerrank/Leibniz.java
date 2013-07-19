ea73ff0ae799418ed655d0135e0529a8ee867cd1
Thys Michels <thysmichels@gmail.com>
2013-07-18 20:53:55 -0700

    Update to HackerRank

--- src/com/thys/michels/hackerrank/Leibniz.java
+++ src/com/thys/michels/hackerrank/Leibniz.java
@@ -8,24 +8,24 @@ import java.util.Scanner;
 
 public class Leibniz {
 	public static void main(String[] args) {
-		String data = "2\n 10\n 20\n";
+		/*String data = "2\n 10\n 20\n";
 		InputStream stdin = System.in;
 		System.setIn(new ByteArrayInputStream(data.getBytes()));
 		Scanner scanner = new Scanner(System.in);
 		scanner.nextLine();
 		System.setIn(stdin);
-		String[] fields = scanner.useDelimiter("\\A").next().trim().split("[\\r\\n]+");
+		String[] fields = scanner.useDelimiter("\\A").next().trim().split("[\\r\\n]+");*/
 		
-		//Scanner scanner = new Scanner(new BufferedInputStream(System.in), "UTF-8");
-		//scanner.nextLine();
-		//String[] fields = scanner.useDelimiter("\\A").next().trim().split("[\\r\\n]+");
+		Scanner scanner = new Scanner(new BufferedInputStream(System.in), "UTF-8");
+		scanner.nextLine();
+		String[] fields = scanner.useDelimiter("\\A").next().trim().split("[\\r\\n]+");
 		for (String field : fields){
 		double calcLeibniz = 1;
 		for (double k = 3, pos = 2; k <= Double.valueOf(field)*2; k+=2, pos++)
 		{
 			calcLeibniz -= Math.pow(-1, pos)/(k);
 		}
-		System.out.println(new DecimalFormat("#.###############").format(calcLeibniz));
+		System.out.println(String.format("%.15f", calcLeibniz));
 		}
 	}
 }

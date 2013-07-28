37dbc399ab2b690365a826a433d1ac564a5ebbbf
Thys Michels <thysmichels@gmail.com>
2013-07-28 00:24:17 -0700

    Update to TurboSort

--- src/com/thys/michels/codechef/TurboSort.java
+++ src/com/thys/michels/codechef/TurboSort.java
@@ -1,17 +1,13 @@
 package com.thys.michels.codechef;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class TurboSort extends PrintWriter {
@@ -65,17 +61,17 @@ public class TurboSort extends PrintWriter {
 	 }
 
 	public static void main(String[] args) {
-		String data = "5\n5\n3\n6\n7\n1\n";
-		System.setIn(new ByteArrayInputStream(data.getBytes()));
+		//String data = "5\n5\n3\n6\n7\n1\n";
+		//System.setIn(new ByteArrayInputStream(data.getBytes()));
 		TurboSort io = new TurboSort(System.in, System.out);
 		int elementsInList = io.getInt();
-		Long[] arrayListLong = new Long[elementsInList];
-		for (int k =0; k< elementsInList; k++){
+		long[] arrayListLong = new long[elementsInList];
+		for (int k = 0; k< elementsInList; k++){
 			arrayListLong[k]=io.getLong();
 		}
-		Arrays.sort(arrayListLong, Collections.reverseOrder());
-		for (Long sortedList : arrayListLong){
-			System.out.println(sortedList);
+		Arrays.sort(arrayListLong);
+		for (long sortedList : arrayListLong){
+			io.println(sortedList);
 		}
 		io.close();
 	}

9e435b708ab8d6271f86ca3fc00ae5f2b4df7719
Thys Michels <thysmichels@gmail.com>
2013-07-28 23:48:42 -0700

    Update to PrimePalindromes

--- src/com/thys/michels/codechef/PrimePalindromes.java
+++ src/com/thys/michels/codechef/PrimePalindromes.java
@@ -1,45 +1,96 @@
 package com.thys.michels.codechef;
 
-import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
-public class PrimePalindromes {
+public class PrimePalindromes extends PrintWriter {
+	private BufferedReader r;
+	private String line;
+	private StringTokenizer st;
+	private String token;
 
+	public PrimePalindromes(InputStream i) {
+		super(new BufferedOutputStream(System.out));
+		r = new BufferedReader(new InputStreamReader(i));
+	}
+	public PrimePalindromes(InputStream i, OutputStream o) {
+		super(new BufferedOutputStream(o));
+		r = new BufferedReader(new InputStreamReader(i));
+	}
+	   
+	public long getLong() {
+		return Long.parseLong(nextToken().trim());
+	 }
+
+	 public String getWord() {
+			return nextToken().trim();
+	 }
+	 
+	 private String nextToken() {
+			String ans = peekToken();
+			token = null;
+			return ans;
+	 }
+	 private String peekToken() {
+			if (token == null) 
+			    try {
+				while (st == null || !st.hasMoreTokens()) {
+				    line = r.readLine();
+				    if (line == null) return null;
+				    st = new StringTokenizer(line);
+				}
+				token = st.nextToken();
+			    } catch (IOException e) { }
+			return token;
+	}
+	
+	public boolean hasMoreTokens() {
+			return peekToken() != null;
+	}
+	
+	public int getInt() {
+		return Integer.parseInt(nextToken());
+	 }
+	
+	public static boolean isPalindrome(long number) {
+	    long original = number;
+	    long reverse = 0;
+	    while (number > 0) {
+	        long remain = number%10;
+	        number = number/10;
+	        reverse = reverse*10+remain;
+	    }
+	    return reverse == original;
+	}
+	
 	public static void main(String[] args) {
 		String data = "99000\n";
 		System.setIn(new ByteArrayInputStream(data.getBytes()));
-		Scanner scanner = new Scanner(new BufferedInputStream(System.in), "UTF-8");
-		long palindromeVal = Long.parseLong(scanner.nextLine());
+		PrimePalindromes io = new PrimePalindromes(System.in, System.out);
+		long palindromeVal = io.getLong();
 		if (palindromeVal>=1 && palindromeVal <=1000000){
 			boolean resultflag = false;
-			Long pcounter = palindromeVal;
 			while (resultflag == false){
-				boolean flagPrime = true;
-				if (pcounter%2==0){
-					flagPrime = false;
-				}
-				else if (flagPrime == true){
-					for (int i = 3; i*i<=pcounter;i+=2){
-						if (pcounter%i==0)
+				if (isPalindrome(palindromeVal)){
+					boolean flagPrime=true;
+					for (int divisor = 2; divisor <= palindromeVal / 2; divisor++){
+						if (palindromeVal % divisor == 0) 
 							flagPrime = false;
 					}
-				}
-				if (flagPrime == true){
-					boolean flagPalindrome = true;
-					char[] pCounterChar = pcounter.toString().toCharArray();
-					for (int start = 0, end=pCounterChar.length-1; start < end;start++, end--){
-						if (pCounterChar[start] != pCounterChar[end]){
-							flagPalindrome = false;
-						}
-					}
-					if (flagPalindrome == true){
-						System.out.println(pcounter);
+					if (flagPrime==true){
+						System.out.println(palindromeVal);
 						resultflag = true;
 					}
 				}
-				pcounter++;
-			}
+				palindromeVal++;
+			}	
 		}
 	}
 }
